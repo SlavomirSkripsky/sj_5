585e3a8b731e7c0579038f889d5335c0403d4778
SlavomirSkripsky <slavkoskripsky@gmail.com>
2025-02-25 15:29:21 +0100

    Prvá verzia

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hlavný nadpis</h1>
+    <?php
+        echo "Výpis";
+        // echo 'Výpis';
+        // echo ("Výpis");
+            $meno = "Peter";
+            echo "$meno <br>";
+            echo "$meno <br>";
+            echo $meno."<br>";
+            //var_dump
+            var_dump($meno);
+
+            //POLIA
+            $farby = array("červená","modrá","zelená"); //INDEXOVANE POLIA
+            echo $farby [0]; //Vypíše: červená
+            echo("<br>");
+
+            foreach($farby as $farba){
+                echo "Farba: ". $farba. "<br>";
+            }
+            foreach($farby as $farba){
+            }
+            //python dictionary  kluc : hodnota
+            $data = array("Peter" => 25,
+                      "Jana" => 30,
+                      "Marek" => 20 
+                    );
+            foreach($data as $name => $age){
+                echo "$name má $age rokov <br>";
+            }
+    ?>
+</body>
+</html>
